5c1de487b7cf86d3c2ad56c5f6b9fd7a3fec2ebc
Vitor Peixoto <vitor-peixoto@outlook.pt>
2019-06-10 18:13:29 +0100

    povoamento

---
+++ part2/povoamento/users.sql
@@ -0,0 +1,10 @@
+INSERT INTO "public"."auth_user" ("id", "password", "last_login", "is_superuser", "username", "first_name", "last_name", "email", "is_staff", "is_active", "date_joined") VALUES ('1', 'pbkdf2_sha256$150000$i3wOqhttA85F$f4FMnK5s99qZvzZ3WegoxmogH9ylhmE9AlyS/2VnsRI=', '2019-06-06 23:36:00.081593+01', 't', 'admin', '', '', 'vitor-peixoto@outlook.pt', 't', 't', '2019-06-06 23:35:17.71104+01'),
+('2', 'pbkdf2_sha256$150000$WYYjjZfgq5T0$ekRuqpIDUP8DJ5EIgI1d3lHHbO6OIIY/saUMvhKVIgc=', NULL, 'f', 'vitorpeixoto', '', '', '', 'f', 't', '2019-06-06 23:37:34.935947+01'),
+('3', 'pbkdf2_sha256$150000$Xts3wTIlKKEd$2zghmsQ/fUylgL4nNaQNDpAl2HT1xdj8zAOGd8dkVJY=', NULL, 'f', 'antoniolopes', '', '', '', 'f', 't', '2019-06-06 23:38:30.268315+01'),
+('4', 'pbkdf2_sha256$150000$wmA6qF4kifau$CN3TAsGLNaqaYXTEuI8t7Hu6JAHaCWYj5qKAwz4TZX8=', NULL, 'f', 'catarinamendes', '', '', '', 'f', 't', '2019-06-06 23:38:46.639358+01'),
+('5', 'pbkdf2_sha256$150000$PCwwv0TYpQbP$uum5VwzszPwh/DnD+IcpXXtzMwyTBSc+6M8mfJgaIfc=', NULL, 'f', 'danielmaia', '', '', '', 'f', 't', '2019-06-06 23:39:21.408394+01');
+
+INSERT INTO "public"."app_user" ("id", "username", "email", "name", "coins", "type") VALUES ('0', 'vitorpeixoto', 'vitor-peixoto@outlook.pt', 'Vitor Peixoto', '32', '0'),
+('1', 'antoniolopes', 'antoniolopes@mail.com', 'António Lopes', '20', '1'),
+('2', 'catarinamendes', 'catarinamendes@mail.com', 'Catarina Mendes', '12', '0'),
+('3', 'danielmaia', 'danielmaia@mail.com', 'Daniel Maia', '35', '1');
