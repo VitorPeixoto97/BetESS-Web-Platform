4b6cf99db9512a27bcefdf9f0e0f818dac77cb61
Daniel Maia <33250763+SilentLynx@users.noreply.github.com>
2019-06-15 04:25:46 +0100

    Histórico de eventos terminados para o admin

--- part2/client/src/router/index.js
+++ part2/client/src/router/index.js
@@ -10,6 +10,7 @@ import Movimentos from '@/components/pages/Movimentos'
 import AdminEventos from '@/components/pages/AdminEventos'
 import AdminEquipas from '@/components/pages/AdminEquipas'
 import AdminCompeticoes from '@/components/pages/AdminCompeticoes'
+import AdminHistorico from '@/components/pages/AdminHistorico'
 
 Vue.use(Router)
 
@@ -59,6 +60,11 @@ export default new Router({
       path: '/admincompeticoes',
       name: 'AdminCompeticoes',
       component: AdminCompeticoes
+    },
+    {
+      path: '/adminhistorico',
+      name: 'AdminHistorico',
+      component: AdminHistorico
     }
   ]
 })
